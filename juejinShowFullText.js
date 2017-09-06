af9f10673e60d759b708c044a5e4eddbaa9db3af
肖 <haseeer@gmail.com>
2017-09-06 15:48:34 +0800

    remove the countdown

--- juejinShowFullText.js
+++ juejinShowFullText.js
@@ -1,27 +1,25 @@
 // ==UserScript==
 // @name         掘金文章免登录展开全文
 // @namespace    haseeer@gmail.com
-// @version      0.1.1
+// @version      0.1.2
 // @description  显示全文
-// @author       shilx
+// @author       泥壕
 // @match        https://juejin.im/post/*
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var timeid;
-    function checkBlock(){
-        timeid=setTimeout(function(){
-            console.log(timeid);
-            if (document.querySelector(".show-full")===null){
-                checkBlock();
-            }else{
-                clearTimeout(timeid);
-                document.querySelector(".show-full").remove();
-                document.querySelector(".show-full-block").remove();
-                document.querySelector(".post-content-container").style.maxHeight="";
-            }
-        },300);
+    function ready(fn) {
+        if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
     }
-    checkBlock();
+    function checkBlock() {
+        document.querySelector(".show-full").remove();
+        document.querySelector(".show-full-block").remove();
+        document.querySelector(".post-content-container").style.maxHeight = "";
+    }
+    ready(checkBlock)
 })();
